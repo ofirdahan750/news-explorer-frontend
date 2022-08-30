d5df0f54e5612026203a610e833dbb49c41e463f
Ofir Dahan <ofirdahan@Ofirs-MacBook-Air.local>
2022-08-30 19:00:11 +0300

    fixing the button the logout

--- src/components/NavHeader/NavHeader.js
+++ src/components/NavHeader/NavHeader.js
@@ -31,21 +31,33 @@ const NavHeader = ({
           Sign in
         </button>
       ) : (
-        <button
-          type="button"
-          onClick={handleLogOutclicked}
-          className="nav-header__btn pulsate-bck nav-header__btn_type_signout btn-link-modifier "
-        >
-          <span className="nav-header__logout-text">{name}</span>
-          <img
-            className="nav-header__logout-icon "
-            src={
-              !isMoblieMenuOpen || location.pathname === "/saved-news"
-                ? require(`../../images/Header/logout_black_theme.svg`).default
-                : require(`../../images/Header/logout_white_theme.svg`).default
-            }
-          />
-        </button>
+        <>
+          <button
+            type="button"
+            className={`nav-header__btn ${
+              location.pathname === "/" && "nav-header__btn_active"
+            } pulsate-bck btn-link-modifier`}
+          >
+            <Link to="/">saved</Link>
+          </button>
+          <button
+            type="button"
+            onClick={handleLogOutclicked}
+            className="nav-header__btn pulsate-bck nav-header__btn_type_signout btn-link-modifier "
+          >
+            <span className="nav-header__logout-text">{name}</span>
+            <img
+              className="nav-header__logout-icon "
+              src={
+                !isMoblieMenuOpen || location.pathname === "/saved-news"
+                  ? require(`../../images/Header/logout_black_theme.svg`)
+                      .default
+                  : require(`../../images/Header/logout_white_theme.svg`)
+                      .default
+              }
+            />
+          </button>
+        </>
       )}
     </nav>
   );
