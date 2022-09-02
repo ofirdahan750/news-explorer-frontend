af023592ee700f02edf57928b249dc46b1e840de
Ofir Dahan <ofirdahan@Ofirs-MacBook-Air.local>
2022-09-02 17:49:05 +0300

    Article fixing the list css

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -1,12 +1,14 @@
+import React, {useState} from "react";
 import "./ArticleCard.css";
 const ArticleCard = ({
   article: {source, title, publishedAt, description, imgUrl, url}
 }) => {
+  const [isButtonHover, setIsButtonHover] = useState(false);
   return (
-    <li className="articles-card fade-in scale-hover">
-      <article className="articles-card_wrapper">
+    <li className="article-card fade-in scale-hover">
+      <article className="article-card_wrapper">
         <div
-          className="articles-card_img-container"
+          className="article-card_img-container"
           role="img"
           aria-label="A photo from the article"
           style={{backgroundImage: `url(${imgUrl})`}}
@@ -17,39 +19,37 @@ const ArticleCard = ({
           }}
         >
           <button
-            className="articles-card__saved-btn btn-link-modifier"
+            className="article-card__saved-btn btn-link-modifier"
             type="button"
             onClick={(e) => {
               e.stopPropagation();
             }}
+            onMouseOver={() => setIsButtonHover(true)}
+            onMouseOut={() => setIsButtonHover(false)}
           >
             <img
               src={
-                require("../../images/Article/bookmark_icons/bookmark-notsaved-gray.svg")
-                  .default
+                isButtonHover
+                  ? require("../../images/Article/bookmark_icons/bookmark-notsaved-black.svg")
+                      .default
+                  : require("../../images/Article/bookmark_icons/bookmark-notsaved-gray.svg")
+                      .default
               }
               alt="Add/Remove to saved article "
-              className="articles-card__saved-btn-icon "
-              onMouseOver={(e) => {
-                e.target.src =
-                  require("../../images/Article/bookmark_icons/bookmark-notsaved-black.svg").default;
-              }}
-              onMouseOut={(e) => {
-                e.target.src =
-                  require("../../images/Article/bookmark_icons/bookmark-notsaved-gray.svg").default;
-              }}
+              className="article-card__saved-btn-icon "
             />
           </button>
+          <div></div>
         </div>
-        <div className="articles-card__txt-container">
-          <h3 className="articles-card__published-date">{publishedAt}</h3>
-          <h2 className="articles-card__title">{title}</h2>
-          <p className="articles-card__description">{description}</p>
+        <div className="article-card__txt-container">
+          <h3 className="article-card__published-date">{publishedAt}</h3>
+          <h2 className="article-card__title">{title}</h2>
+          <p className="article-card__description">{`${description}${description}`}</p>
           <a
             rel="noreferrer"
             target="_black"
             href={url}
-            className="articles-card__source btn-link-modifier"
+            className="article-card__source btn-link-modifier"
           >
             {source}
           </a>
