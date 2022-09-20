67e0fd44aa50b460d4464c9b7df9ae9e2c1a3558
Ofir Dahan <ofirdahan@Ofirs-MacBook-Air.local>
2022-09-20 16:08:49 +0300

    Fixing the overflow toggle on moblie

--- src/store/actions/loadingAction.js
+++ src/store/actions/loadingAction.js
@@ -1,5 +1,5 @@
 export const setLoading = (isLoading) => {
-  document.querySelector(".page").style.overflow = isLoading
+  document.querySelector(".page").style.overflowx = isLoading
     ? "hidden"
     : "unset";
   return {type: "SET_LOADING", payLoad: {isLoading: isLoading}};
