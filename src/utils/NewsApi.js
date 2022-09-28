aab85c297a7389873752911569e8077304e8d1f0
Ofir Dahan <ofirdahan@Ofirs-MacBook-Air.local>
2022-09-28 18:45:24 +0300

    Readme edited

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -1,4 +1,4 @@
-// import DemoSearchData from "../DemoData/DemoSearchData.json";
+import DemoSearchData from "../DemoData/DemoSearchData.json";
 class NewsApi {
   constructor({baseUrl, from, to, pageSize}) {
     this._baseUrl = baseUrl;
@@ -16,13 +16,13 @@ class NewsApi {
   };
 
   getSearchArticles = async ({searchParmas, apiKey}) => {
-    return await this._onHttpRequest(
-      `${this._baseUrl}/everything?q=${searchParmas}&apiKey=${apiKey}&from=${this._from}&to=${this._to}&pageSize=${this._pageSize}`
-    );
+    // return await this._onHttpRequest(
+    //   `${this._baseUrl}/everything?q=${searchParmas}&apiKey=${apiKey}&from=${this._from}&to=${this._to}&pageSize=${this._pageSize}`
+    // );
     //for Testing and dev on the server
     // DemoSearchData.totalResults = 0;
     // return Promise.resolve([]);
-    // return Promise.resolve(DemoSearchData);
+    return Promise.resolve(DemoSearchData);
     // return Promise.reject();
   };
 }
