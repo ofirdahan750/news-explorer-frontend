f58fec3a0369979bceb64b737d43ef3cc7fa3cba
Ofir Dahan <ofirdahan@Ofirs-MacBook-Air.local>
2022-09-29 14:24:19 +0300

    Remove scroll on moblie + fixing margin on cards

--- src/components/AboutAuthor/AboutAuthor.js
+++ src/components/AboutAuthor/AboutAuthor.js
@@ -10,7 +10,7 @@ const AboutAuthor = () => {
       <article className="about-author__article">
         <h2 className="about-author__headling">About the author</h2>
         <p className="about-author__paragraph" style={{margin: "0 0 10px"}}>
-          Hi! My name is Ofir Dahan. I'm a Full-Stack Developer with expertise
+          Hi! My name is Ofir Dahan. I'm a Full-Stack Developer with experience
           in React, Angular, TypeScript, Design Patterns, and more.
         </p>
         <p className="about-author__paragraph">
